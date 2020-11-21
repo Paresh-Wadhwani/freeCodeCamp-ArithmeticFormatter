9531287d1873c2e0b8ad891b5d7fe14b57c90a7d
parresh77 <parresh77@gmail.com>
2020-11-21 15:19:30 +0530

    Update arithmetic_arranger.py with complete solution.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,65 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, boolarg = False):
+  if len(problems) > 5:
+    return "Error: Too many problems."
+  if str(problems).find('*') > 0 or str(problems).find('/') > 0:
+    return "Error: Operator must be '+' or '-'."
 
+  operand1s = []
+  operators = []
+  operand2s = []
+  maxlens = []
+  ans = []
+  dasharr = []
+  op1 = ""
+  op2 = ''
+  ansstr = ''
+  dashstr = ''
+  
+  for i in problems:
+    x = i.find(' ')
+    y = i.find(' ', x + 1)
+    operand1 = i[:x]
+    operator = i[x + 1:y]
+    operand2 = i[y + 1:]
+    if operand1.isnumeric() == False or operand2.isnumeric() == False:
+      return "Error: Numbers must only contain digits."
+    if len(operand1) > 4 or len(operand2) > 4:
+      return "Error: Numbers cannot be more than four digits."
+    maxlen = max(len(operand1), len(operand2))
+    maxlens.append(maxlen)
 
-    return arranged_problems
+    o1 = ' '*(maxlen - len(operand1)) + operand1
+    o2 = ' '*(maxlen - len(operand2)) + operand2
+
+    operand1s.append(o1)
+    operators.append(operator)
+    operand2s.append(o2)
+    an = str(int(operand1) + int(operand2)) if operator == '+' else str(int(operand1) - int(operand2))
+    an = ' '*(maxlen + 2 - len(an)) + an
+    ans.append(an)
+    dasharr.append('-'*(maxlen+2))
+
+  for j in range(0, len(problems)):
+    if op1 == '':
+      op1 = '  ' + operand1s[j]
+    else:
+      op1 = op1 + '      ' + operand1s[j]
+
+    if op2 == '':
+      op2 = operators[j] + ' ' + operand2s[j]
+    else:
+      op2 = op2 + '    ' + operators[j] + ' ' + operand2s[j]
+
+    if ansstr == '':
+      ansstr = ans[j]
+    else:
+      ansstr = ansstr + '    ' + ans[j]
+    
+    if dashstr == '':
+      dashstr = dasharr[j]
+    else:
+      dashstr = dashstr + '    ' + dasharr[j]
+
+  arranged_problems = (op1 + '\n' + op2 + '\n' + dashstr + '\n' + ansstr) if boolarg else (op1 + '\n' + op2 + '\n' + dashstr)
+
+  return arranged_problems
